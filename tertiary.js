445970cc8e5d260289af5d20601b302c79207246
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-05-31 11:05:26 -0400

    Update tertiary.js

--- tertiary.js
+++ tertiary.js
@@ -8,19 +8,19 @@ const prompt = require("prompt-sync")({sigint: true});
 
 let c_or_d = prompt("Input 1 or 2 colors: red, blue, yellow, violet, orange, green, black, bluegreen, yellowgreen, yelloworange, redorange, blueviolet, redviolet. ").toLowerCase();
 
-let r = 'Red';
-let b = 'Blue';
-let y = 'Yellow';
-let v = 'Violet';
-let o = 'Orange';
-let g = 'Green';
-let bl = 'Black';
-let bg = 'Bluegreen';
-let yg = 'Yellowgreen';
-let yo = 'Yelloworange';
-let ro = 'Redorange';
-let bv = 'Blueviolet';
-let rv = 'Redviolet';
+let r = 'red';
+let b = 'blue';
+let y = 'yellow';
+let v = 'violet';
+let o = 'orange';
+let g = 'green';
+let bl = 'black';
+let bg = 'bluegreen';
+let yg = 'yellowgreen';
+let yo = 'yelloworange';
+let ro = 'redorange';
+let bv = 'blueviolet';
+let rv = 'redviolet';
 
 if (c_or_d.includes('red') || c_or_d.includes('yellow') || c_or_d.includes('blue')  || c_or_d.includes('violet') || c_or_d.includes('orange') || c_or_d.includes('green') || c_or_d.includes('black') || c_or_d.includes('bluegreen') || c_or_d.includes('yellowgreen') || c_or_d.includes('yelloworange') || c_or_d.includes('redorange') || c_or_d.includes('blueviolet') || c_or_d.includes('redviolet')){
     if (c_or_d.includes('red') && c_or_d.includes('blue')){
@@ -43,20 +43,6 @@ if (c_or_d.includes('red') || c_or_d.includes('yellow') || c_or_d.includes('blue
         console.log("The primary color " + b + " and the secondary color " + v + " make the tertiary color " + bv + ".");
     } else if (c_or_d.includes('red') && c_or_d.includes('violet')){
         console.log("The primary color " + r + " and the secondary color " + v + " make the tertiary color " + rv + ".");
-    } else if (c_or_d.includes('red') && (c_or_d.includes('bluegreen') || c_or_d.includes('yellowgreen') || c_or_d.includes('yelloworange') || c_or_d.includes('redorange') || c_or_d.includes('blueviolet') || c_or_d.includes('redviolet'))){
-        console.log("This calculator doesn't mix primary or secondry colors with tertiary colors. Try the primary and secondary colors like " + r + ", " + b + ", " + y + ", " + v + ", " + o + " or " + g + ".");
-    } else if (c_or_d.includes('blue') && (c_or_d.includes('bluegreen') || c_or_d.includes('yellowgreen') || c_or_d.includes('yelloworange') || c_or_d.includes('redorange') || c_or_d.includes('blueviolet') || c_or_d.includes('redviolet'))){
-        console.log("This calculator doesn't mix primary or secondry colors with tertiary colors. Try the primary and secondary colors like " + r + ", " + b + ", " + y + ", " + v + ", " + o + " or " + g + ".");
-    } else if (c_or_d.includes('yellow') && (c_or_d.includes('bluegreen') || c_or_d.includes('yellowgreen') || c_or_d.includes('yelloworange') || c_or_d.includes('redorange') || c_or_d.includes('blueviolet') || c_or_d.includes('redviolet'))){
-        console.log("This calculator doesn't mix primary or secondry colors with tertiary colors. Try the primary and secondary colors like " + r + ", " + b + ", " + y + ", " + v + ", " + o + " or " + g + ".");
-    } else if (c_or_d.includes('green') && c_or_d.includes('bluegreen') || c_or_d.includes('green') && c_or_d.includes('yellowgreen') || c_or_d.includes('green') && c_or_d.includes('yelloworange') || c_or_d.includes('green') && c_or_d.includes('redorange') || c_or_d.includes('green') && c_or_d.includes('blueviolet') || c_or_d.includes('green') && c_or_d.includes('redviolet')){
-        console.log("This calculator doesn't mix primary or secondry colors with tertiary colors. Try the primary and secondary colors like " + r + ", " + b + ", " + y + ", " + v + ", " + o + " or " + g + ".");
-    } else if (c_or_d.includes('orange') && (c_or_d.includes('bluegreen') || c_or_d.includes('yellowgreen') || c_or_d.includes('yelloworange') || c_or_d.includes('redorange') || c_or_d.includes('blueviolet') || c_or_d.includes('redviolet'))){
-        console.log("This calculator doesn't mix primary or secondry colors with tertiary colors. Try the primary and secondary colors like " + r + ", " + b + ", " + y + ", " + v + ", " + o + " or " + g + ".");
-    } else if (c_or_d.includes('violet') && (c_or_d.includes('bluegreen') || c_or_d.includes('yellowgreen') || c_or_d.includes('yelloworange') || c_or_d.includes('redorange') || c_or_d.includes('blueviolet') || c_or_d.includes('redviolet'))){
-        console.log("This calculator doesn't mix primary or secondry colors with tertiary colors. Try the primary and secondary colors like " + r + ", " + b + ", " + y + ", " + v + ", " + o + " or " + g + ".");
-    } else if (c_or_d.includes('black') && (c_or_d.includes('bluegreen') || c_or_d.includes('yellowgreen') || c_or_d.includes('yelloworange') || c_or_d.includes('redorange') || c_or_d.includes('blueviolet') || c_or_d.includes('redviolet') || c_or_d.includes('red') || c_or_d.includes('yellow') || c_or_d.includes('blue')  || c_or_d.includes('violet') || c_or_d.includes('orange') || c_or_d.includes('green') || c_or_d.includes('black'))){
-        console.log("For this calculator, anything with " + bl + " is still " + bl + ". Try the primary and secondary colors like " + r + ", " + b + ", " + y + ", " + v + ", " + o + " or " + g + ".");
     } else if (c_or_d.includes('red')){
         console.log(r + " stands alone. It's a primary color.")
     } else if (c_or_d.includes('blue')){
@@ -71,18 +57,32 @@ if (c_or_d.includes('red') || c_or_d.includes('yellow') || c_or_d.includes('blue
         console.log("The secondary color " + g + " is made from primary colors " + y + " and " + b + ".");
     } else if (c_or_d.includes('black')){
         console.log("The secondary color " + bl + " is made from all the primary colors: " + b + ", " + y + " and " + r + ".");
-    } else if (c_or_d.includes('blue-green')){
+    } else if (c_or_d === bg){
         console.log("The tertiary color " + bg + " is made from primary color " + b + " and secondary color " + g + ".");
-    } else if (c_or_d.includes('yellow-green')){
+    } else if (c_or_d === yg){
         console.log("The tertiary color " + yg + " is made from primary color " + y + " and secondary color " + g + ".")
-    } else if (c_or_d.includes('yellow-orange')){
+    } else if (c_or_d === yo){
         console.log("The tertiary color " + yo + " is made from primary color " + y + " and secondary color " + o + ".")
-    } else if (c_or_d.includes('red-orange')){
+    } else if (c_or_d === ro){
         console.log("The tertiary color " + ro + " is made from primary color " + r + " and secondary color " + o + ".")
-    } else if (c_or_d.includes('blue-violet')){
+    } else if (c_or_d === bv){
         console.log("The tertiary color " + bv + " is made from primary color " + b + " and secondary color " + v + ".")
-    } else if (c_or_d.includes('red-violet')){
+    } else if (c_or_d === rv){
         console.log("The tertiary color " + rv + " is made from primary color " + r + " and secondary color " + v + ".")
+    } else if (c_or_d === r && (c_or_d === bg|| c_or_d === yg || c_or_d === yo || c_or_d === ro || c_or_d === bv || c_or_d === rv)){
+        console.log("This calculator doesn't mix primary or secondry colors with tertiary colors. Try the primary and secondary colors like " + r + ", " + b + ", " + y + ", " + v + ", " + o + " or " + g + ".");
+    } else if (c_or_d === b && (c_or_d === bg|| c_or_d === yg || c_or_d === yo || c_or_d === ro || c_or_d === bv || c_or_d === rv)){
+        console.log("This calculator doesn't mix primary or secondry colors with tertiary colors. Try the primary and secondary colors like " + r + ", " + b + ", " + y + ", " + v + ", " + o + " or " + g + ".");
+    } else if (c_or_d.includes('yellow') && (c_or_d === bg|| c_or_d === yg || c_or_d === yo || c_or_d === ro || c_or_d === bv || c_or_d === rv)){
+        console.log("This calculator doesn't mix primary or secondry colors with tertiary colors. Try the primary and secondary colors like " + r + ", " + b + ", " + y + ", " + v + ", " + o + " or " + g + ".");
+    } else if (c_or_d.includes('green') && (c_or_d === bg|| c_or_d === yg || c_or_d === yo || c_or_d === ro || c_or_d === bv || c_or_d === rv)){
+        console.log("This calculator doesn't mix primary or secondry colors with tertiary colors. Try the primary and secondary colors like " + r + ", " + b + ", " + y + ", " + v + ", " + o + " or " + g + ".");
+    } else if (c_or_d.includes('orange') && (c_or_d === bg|| c_or_d === yg || c_or_d === yo || c_or_d === ro || c_or_d === bv || c_or_d === rv)){
+        console.log("This calculator doesn't mix primary or secondry colors with tertiary colors. Try the primary and secondary colors like " + r + ", " + b + ", " + y + ", " + v + ", " + o + " or " + g + ".");
+    } else if (c_or_d.includes('violet') && (c_or_d === bg|| c_or_d === yg || c_or_d === yo || c_or_d === ro || c_or_d === bv || c_or_d === rv)){
+        console.log("This calculator doesn't mix primary or secondry colors with tertiary colors. Try the primary and secondary colors like " + r + ", " + b + ", " + y + ", " + v + ", " + o + " or " + g + ".");
+    } else if (c_or_d.includes('black') && (c_or_d === bg|| c_or_d === yg || c_or_d === yo || c_or_d === ro || c_or_d === bv || c_or_d === rv || c_or_d.includes('red') || c_or_d.includes('yellow') || c_or_d.includes('blue')  || c_or_d.includes('violet') || c_or_d.includes('orange') || c_or_d.includes('green') || c_or_d.includes('black'))){
+        console.log("For this calculator, anything with " + bl + " is still " + bl + ". Try the primary and secondary colors like " + r + ", " + b + ", " + y + ", " + v + ", " + o + " or " + g + ".");
     } else{
         console.log("Something went wrong. Try again")
     }
